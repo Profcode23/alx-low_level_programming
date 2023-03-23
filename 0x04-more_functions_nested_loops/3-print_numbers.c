dc6b99339521286c2419c3f130ae1c0197c27662
Profcode23 <imwangi31@gmail.com>
2023-03-23 12:40:09 -0700

    Task 3 final

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - list numbers from 0 to 9
+ * print_numbers - List number 0 to 9
  * Return: void
  */
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <='9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
