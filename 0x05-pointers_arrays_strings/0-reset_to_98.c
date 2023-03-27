dcc8004b3ab92ea87791ab248df93c032658d30c
Profcode23 <imwangi31@gmail.com>
2023-03-27 13:46:17 -0700

    Task 1 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that makes a pointer point to int with the value 98
+ * @n: int to be pointed to
+ * return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
