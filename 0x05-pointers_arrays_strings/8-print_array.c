06e858971aa9390df22c3ed284d1dfe43f398ab2
Profcode23 <imwangi31@gmail.com>
2023-03-27 15:29:14 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - display number of elements in an array followed by a new line
+ * @a: Testcase string character
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int num;
+
+	for (num = 0; num < n; num++)
+		if (num != n - 1)
+			printf("%d, ", a[num]);
+		else
+			printf("%d", a[num]);
+	printf("\n");
+}
