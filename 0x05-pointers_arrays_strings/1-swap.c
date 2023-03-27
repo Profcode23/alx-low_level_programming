b70e6b3e07e50a27ac614c9c9c69a91d6c650232
Profcode23 <imwangi31@gmail.com>
2023-03-27 13:58:25 -0700

    Task 1 swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - will swap the values of two integers
+ * @a: first number
+ * @b: second number
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int swap_num;
+	swap_num = *b;
+	*b = *a;
+	*a = swap_num;
+}
