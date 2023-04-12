b870b94721165d10660973634aec2953570dd2ce
Profcode23 <imwangi31@gmail.com>
2023-04-12 13:05:48 -0700

    Task updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,46 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
+ * str_concat - Concatenate 2 strings and return pointer to new addrss
+ * @s1: the 1st string
+ * @s2: the 2nd string
  *
- * Return: a pointer to a newly allocated space in memory 
+ * Return: Null if fail, else return new address
  */
+
 char *str_concat(char *s1, char *s2)
 {
-  int i, j, len1, len2, len;
-  char *result;
+	int content_s1, content_s2, length_s1, length_s2, new_length;
+	char *concated;
 
-  len1 = len2 = 0;
+	length_s1 = 0;
+	length_s2 = 0;
 
-  if (s1 != NULL)
-  {
-     i = 0;
-     while (s1[i++] != '\0')
-        len1++;
-  }
+	if (s1 != NULL)
+	{
+		content_s1 = 0;
+		while (s1[content_s1++] != '\0')
+			length_s1++;
+	}
 
-  if (s2 != NULL)
-  {
-     i = 0;
-     while (s2[i++] != '\0')
-        len2++;
-  }
+	if (s2 != NULL)
+	{
+		content_s1 = 0;
+		while (s2[content_s1++] != '\0')
+			length_s2++;
+	}
 
-  len = len1 + len2;
-  result = (char *)malloc(sizeof(char) * (len + 1));
-  if (result == NULL)
-     return (NULL);
+	new_length = length_s1 + length_s2;
+	concated = malloc(sizeof(char) * (new_length + 1));
+	if (concated == NULL)
+		return (NULL);
 
-  for (i = 0; i < len1; i++)
-     result[i] = s1[i];
-  for (j = 0; j < len2; j++, i++)
-     result[i] = s2[j];
-  result[len] = '\0';
+	for (content_s1 = 0; content_s1 < length_s1; content_s1++)
+		concated[content_s1] = s1[content_s1];
+	for (content_s2 = 0; content_s2 < length_s2; content_s2++, content_s1++)
+		concated[content_s1] = s2[content_s2];
+	concated[new_length] = '\0';
 
-  return (result);
+	return (concated);
 }
